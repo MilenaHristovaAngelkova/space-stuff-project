d6d463ca22cb8e7397c03ec187152e5188a444fd
MilenaHristovaAngelkova <light_milena@abv.bg>
2021-08-09 14:14:31 +0300

    updated the way the details are displayed, sorted out a bug, added loading info

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,15 @@
+import { useState } from "react";
+
 const Home = () => {
+    const [isPendingImg, setIsPendingImg] = useState(false);
+    const [isPendingData, setIsPendingData] = useState(false);
+
     const handleOnClickImage = () => {
+        setIsPendingImg(true);
         fetch("https://api.nasa.gov/planetary/apod?api_key=9YUajepmhWTLLO1dz38SxEeKsWa8KyzPDKK66Bep")
         .then(res => {
             if(!res.ok){
+                setIsPendingImg(false);
                 throw new Error("Could not fetch the data from this source.")
             }
             return res.json();
@@ -17,6 +24,7 @@ const Home = () => {
                 </div>
             `
            document.querySelector(".content-image").innerHTML = template;
+           setIsPendingImg(false);
         })
         .catch(err => {
             const template = `
@@ -25,26 +33,46 @@ const Home = () => {
             </div>
             `
             document.querySelector(".content-image").innerHTML = template;
+            setIsPendingImg(false);
         })        
     }
 
     const handleOnClickData = () => {
+        setIsPendingData(true);
         fetch("https://ssd-api.jpl.nasa.gov/cad.api")
         .then(res => {
             if (!res.ok) {
-                throw new Error("Could not fetch the people details from this source.")
+                setIsPendingData(false);
+                throw new Error("Could not fetch the people details from this source.");
             }
             return res.json();
         })
         .then(details => {
             const title = `<h3>NEO Earth close-approaches of less than 0.05 au in the next 60 days</h3>`;
             const total = `<h4>Total expected close approaches: ${details.count}</h4>`;
+            const dataTable = `
+            <table>
+                <thead>
+                    <th>Asteroid/Comet Designation</th>
+                    <th>Date</th>
+                </thead>
+                <tbody class="data-details">
+                <tbody>
+            </table>
+            `
+            document.querySelector(".content-data").innerHTML = title + total + dataTable;
             
             const template = details.data.map(el => {
-                return `<li>Asteroid/comet designation <strong>"${el[0]}"</strong> will be close to Earth on <strong>${el[3]}</strong>.</li>`
+                return `
+                <tr>
+                    <td>${el[0]}</td>
+                    <td>${el[3]}</td>
+                </tr>
+                `
             });
 
-            document.querySelector(".content-data").innerHTML = title + total + template;
+            document.querySelector(".data-details").innerHTML = template.join("");
+            setIsPendingData(false);
         })
         .catch(err => {
             const template = `
@@ -53,13 +81,15 @@ const Home = () => {
             </div>
             `
             document.querySelector(".content-data").innerHTML = template;
+            setIsPendingData(false);
         })   
     }
 
     return (
         <div className="home">
-            <button id="get-image" onClick={handleOnClickImage}>Get image of the day</button>
-            <button id="get-story" onClick={handleOnClickData}>Get asteroids and comets close-approach data</button>
+            <button id="get-image" onClick={handleOnClickImage}>{isPendingImg === false ? "Get image of the day" : "Loading..."}</button>
+            <button id="get-story" onClick={handleOnClickData}>{isPendingData === false ? 
+            "Get asteroids and comets close-approach data" : "Loading..."}</button>
             <article className="content-image">                
             </article>
             <article className="content-data">
